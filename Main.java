0b174b3e22f515977db85279e2e03e38d9ea38af
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-05-29 19:36:22 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import mymath.CalcUI;
 
 public class Main {
+    /**
+     * Runs the PolyCalc program.
+     * @param args  String[] : the arguments taken by the program
+     */
     public static void main(String[] args) {
-        CalcUI display = new CalcUI();
-        display.Gui();
+        CalcUI calc = new CalcUI();
+        calc.displayGUI();
     }
 }
