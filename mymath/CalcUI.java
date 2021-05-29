640e72bb2f17c871115c7d7742c592be816f43ec
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-05-29 19:34:31 -0400

    Update CalcUI.java

--- mymath/CalcUI.java
+++ mymath/CalcUI.java
@@ -3,14 +3,22 @@ package mymath;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * This class displays the user-interface for PolyCalc outputs.
+ * @author Trong Nguyen
+ * @version 1.0
+ */
 public class CalcUI {
-    public void Gui() {
+    /**
+     * This method displays the GUI.
+     */
+    public void displayGUI() {
         System.out.println("Welcome to the PolyCalc App!");
-        System.out.println("""
-                This application has three basic polynomial calculus functions:
-                 -> Evaluate\s
-                 -> Derivative\s
-                 -> Integrate""");
+        System.out.println("" +
+                "This application has three basic polynomial calculus functions:\n" +
+                "-> Evaluate\n" +
+                "-> Derivative\n" +
+                "-> Integrate");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the coefficients of the polynomial separated by space: ");
         System.out.println("For example: 3.0x^2 + 2.0x^1 + 1.0, would be: 3 2 1");
@@ -39,43 +47,48 @@ public class CalcUI {
                 validFunction = false;
             }
             while (validPoly && !validFunction) {
-                System.out.println("""
-                    Select one of the following functions:
-                     [e] Evaluate\s
-                     [d] Derivative\s
-                     [i] Integrate\s
-                     [b] Enter new polynomial\s
-                     [x] Exit the program""");
+                System.out.println("Select one of the following functions:\n " +
+                        "[e] Evaluate\n " +
+                        "[d] Derivative\n " +
+                        "[i] Integrate\n " +
+                        "[b] Enter new polynomial\n " +
+                        "[x] Exit the program");
                 System.out.print(">>> ");
                 sc = new Scanner(System.in);
                 String function = sc.nextLine();
                 switch (function) {
-                    case "e" -> {
+                    case "e": {
                         System.out.print("x = ");
                         int x = sc.nextInt();
                         double result = poly.evaluate(x);
                         System.out.println("Evaluate:"); poly.printPoly();
                         System.out.println("with x = " + x + " >>> " + result);
+                        break;
                     }
-                    case "d" -> {
+                    case "d": {
                         poly = PolyCalc.differentiate(poly);
                         System.out.print("Differentiate: ");
                         poly.printPoly();
+                        break;
                     }
-                    case "i" -> {
+                    case "i": {
                         poly = PolyCalc.integrate(poly);
                         System.out.print("Integrated: ");
                         poly.printPoly();
+                        break;
                     }
-                    case "b" -> {
+                    case "b": {
                         validFunction = true;
                         validPoly = false;
+                        break;
                     }
-                    case "x" -> {
+                    case "x": {
                         System.out.println("Exiting. Good-bye!");
                         System.exit(1);
                     }
-                    default -> System.out.println("Must select a function!");
+                    default:
+                        System.out.println("Must select a function!");
+                        break;
                 }
             }
         }
